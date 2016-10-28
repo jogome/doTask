aeb45921864b375b2a1dd4ee41905fce26278815
jogome <johngraceair@gmail.com>
2016-10-28 10:10:15 -0100

    taskWindow 2

--- client/dotask.js
+++ client/dotask.js
@@ -14,6 +14,31 @@ Template.taskWindow.helpers({
 	}
 });
 
+
+Template.dateAndTime_Starting_Ending.helpers({
+	"priorityLevel": function() {
+		var taskId = this._id;
+		var taskPriorityLevel = Tasks.findOne({_id: taskId}).priority;
+		if (taskPriorityLevel == 'normal') {
+			return "high-priority";
+		}
+		else {
+			return  "normal-priority";
+		} 
+	 },
+	 "checkBox": function() {
+		var taskId = this._id;
+		//console.log("The value= "+Tasks.findOne({_id: taskId}).boxChecked);
+		var checkBoxValue = Tasks.findOne({_id: taskId}).boxChecked;
+		if (checkBoxValue == true) {
+			console.log("Box value = "+checkBoxValue);
+			return "checked";
+		}
+		else {
+			return "";
+		}
+	 }
+});
 	
 /////////
 //// EVENTS
@@ -33,7 +58,14 @@ Template.getTasksForm.events({
 		// Inserts the task title and the task detail in the database
 		Tasks.insert({
 			taskTitle: newTaskTitle,
-			taskDetail: newTaskDetail
+			taskDetail: newTaskDetail,
+			boxChecked: false,
+			priority: 'high',
+			time: formatedTime,
+			startDate: "",
+			startTime: "",
+			endDate: "",
+			endTime: ""
 		});
 		
 		$("form").toggle('slow');
@@ -69,4 +101,72 @@ Template.taskWindow.events({
 				return false; 
 	  }
 	}
+	
+});
+
+/* Formating the date and time*/
+var dateNow = new Date();
+	// formating the date
+	var yearMonthDay = dateNow.toLocaleDateString();
+	var time = dateNow.toLocaleTimeString();
+	var formatedTime = yearMonthDay + " at " + time;
+
+
+Template.dateAndTime_Starting_Ending.events({
+	"change input[type=date].startDate": function(event) {
+			var taskId = this._id;
+			var selectedStartDate = event.target.value;
+			console.log("Starting date preview = "+selectedStartDate);
+			//alert(selectedStartDate);
+			Tasks.update({_id: taskId}, {$set:{startDate: selectedStartDate}});
+		},
+		"change input[type=date].endDate": function(event) {
+			var taskId = this._id;
+			// //Session.set("taskId_from_endDate", taskId);
+			var selectedEndDate = event.target.value;
+			console.log("Ending date preview = "+selectedEndDate);
+			//alert(selectedStartDate);
+			Tasks.update({_id: taskId}, {$set:{endDate: selectedEndDate}});
+			// //Session.set("endDate_for_thisId", selectedEndDate);
+		},
+		"change input[type=time].startTime": function(event) {
+			var taskId = this._id;
+			var selectedStartTime = event.target.value;
+			console.log("starting time = "+selectedStartTime);
+			//alert(selectedStartDate);
+			Tasks.update({_id: taskId}, {$set:{startTime: selectedStartTime}});
+		},
+		"change input[type=time].endTime": function(event) {
+			var taskId = this._id;
+			var selectedEndTime = event.target.value;
+			console.log("ending time = "+selectedEndTime);
+			//alert(selectedStartDate);
+			Tasks.update({_id: taskId}, {$set:{endTime: selectedEndTime}});
+		},
+		"click .js-task-priority": function() {
+			console.log("You clicked on me!");
+			var taskId = this._id;
+			//Session.set("actual_Id", taskId);
+			console.log(taskId);
+			var taskPriorityLevel = Tasks.findOne({_id: taskId}).priority;
+			if (taskPriorityLevel == "normal") {
+				Tasks.update({_id: taskId}, {$set: {priority: 'high'}});
+			}
+			else {
+				Tasks.update({_id: taskId}, {$set: {priority: 'normal'}});
+			}	
+				 
+		},
+		"click .checkbox": function() {
+			console.log("You clicked on a checkbox!");
+			console.log("ID"+this._id);
+			var taskId = this._id;
+			var checkBoxValue = Tasks.findOne({_id: taskId}).boxChecked;
+			if (checkBoxValue == false) {
+				Tasks.update({_id: taskId}, {$set: {boxChecked: true}});
+			}
+			else {
+				Tasks.update({_id: taskId}, {$set: {boxChecked: false}});
+			}	
+		}	
 });
